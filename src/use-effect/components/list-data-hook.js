d15b819a27eb750cee40611dd0f6206b9edb53fd
zassory <nicolas.programador@gmail.com>
2023-02-21 15:59:58 -0300

    hook useEffect uso intermedio

--- src/use-effect/components/list-data-hook.js
+++ src/use-effect/components/list-data-hook.js
@@ -1,10 +1,22 @@
 import React, { useEffect, useState } from 'react';
-
 import {myApi} from '../my-api';
+
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 export const ListDataHook = () => {
     
     const [data,setData] = useState([]);
+    const [userId,setUserId] = useState(1);    
+
+    const handleUserId = () => {
+        const randomId = getRandomInt(1,5);
+        setUserId(randomId);
+        //this.setState({userId: randomId});
+    };
     
     // state = {
     //     data: [],
@@ -12,17 +24,27 @@ export const ListDataHook = () => {
 
     useEffect(()=> {
         async function fetchData() {
-            const data = await myApi.fakeFetch();
+            const data = await myApi.fakeFetch(userId);
             setData(data);
         }
         fetchData();
-    },[]);
+    },[userId]);
 
     // async componentDidMount() {
     //     const data = await myApi.fakeFetch();
     //     this.setState({data});
     // }
     
-    return data.map(({label}) => <p key={label}>{label}</p>);      
+    return (
+        <>
+            <h3>User ID: {userId}</h3>
+            {data.map(({label}) => (
+                <p key={label}>{label}</p>
+            ))}
+            <button onClick={handleUserId}>Update user id</button>
+        </>
+    );
+    
+    //data.map(({label}) => <p key={label}>{label}</p>);      
     
 };
